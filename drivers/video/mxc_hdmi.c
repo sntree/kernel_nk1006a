d7d68ac0740008468c859a9683b724b17d8eb8c0
sn <sntree@gmail.com>
2014-12-18 13:49:19 +0800

    hdmi is ok, do not comment it

--- drivers/video/mxc_hdmi.c
+++ drivers/video/mxc_hdmi.c
@@ -1936,15 +1936,11 @@ static void mxc_hdmi_cable_connected(struct mxc_hdmi *hdmi)
 	edid_status = mxc_hdmi_read_edid(hdmi);
 
 	/* Read EDID again if first EDID read failed */
-#if 0	
 	if (edid_status == HDMI_EDID_NO_MODES ||
 			edid_status == HDMI_EDID_FAIL) {
 		dev_info(&hdmi->pdev->dev, "Read EDID again\n");
 		edid_status = mxc_hdmi_read_edid(hdmi);
 	}
-#else	
-	dev_info(&hdmi->pdev->dev, "Read EDID again %d\n", edid_status);
-#endif
 
 	/* HDMI Initialization Steps D, E, F */
 	switch (edid_status) {
