fddb31ee5ffd0ff5e6e9e155a23f643772fac83b
sn <sntree@gmail.com>
2015-06-03 18:01:26 +0800

    fix pwn device name issue: prefix only, such as DISP4

--- drivers/video/backlight/pwm_bl.c
+++ drivers/video/backlight/pwm_bl.c
@@ -74,6 +74,7 @@ static char backlight_fb[32] = {0};
 static int __init backlight_setup(char *str)
 {
 	strncpy(backlight_fb, str, sizeof(backlight_fb));
+	//printk("### got pwm_backlight: %s\n ", backlight_fb);
 	return 1;
 }
 __setup("pwm_backlight=", backlight_setup);
@@ -82,19 +83,21 @@ static int pwm_backlight_check_fb(struct backlight_device *bl,
 					struct fb_info *info)
 {
 	char *id = info->fix.id;
-	
-	if (backlight_fb != NULL && backlight_fb[0] != 0) {
-		if (!strncmp(id, backlight_fb, sizeof(backlight_fb)))
-			return 1;
-		else
-			return 0;	
-	}
-	
+
 	if (!strcmp(id, "DISP3 BG") ||
 		!strcmp(id, "DISP3 BG - DI1") ||
 		!strcmp(id, "DISP4 BG") ||
-		!strcmp(id, "DISP4 BG - DI1"))
-	    return 1;
+		!strcmp(id, "DISP4 BG - DI1")) {
+
+		if (backlight_fb != NULL && backlight_fb[0] != 0) {
+			if (!strncmp(id, backlight_fb, 5))
+				return 1;
+			else
+				return 0;
+		}
+
+		return 1;
+	}
 	else
 		return 0;
 }
